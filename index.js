9a100c2aff7630dfe0060814a427bf1aee260b56
Corbin McLeod <41881731+Ice-Media@users.noreply.github.com>
2018-08-27 17:59:59 -0600

    Added Level System

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({ disableEveryone: true });
 bot.commands = new Discord.Collection();
+let xp = require("./xp.json")
+let purple = botconfig.purple;
 
 fs.readdir("./Commands", (err, files) => {
   if (err) console.log(err);
@@ -29,6 +31,37 @@ bot.on("ready", () => {
 bot.on("message", message => {
   if (message.author.bot) return;
   if (message.channel.type === "dm") return;
+
+  //Level System START
+
+  let xpAdd = Math.floor(Math.random() * 7) + 8;
+  console.log(xpAdd);
+
+  if(!xp[message.author.id]){
+    xp[message.author.id] = {
+      xp: 0,
+      level: 1
+    };
+  }
+
+  let curxp = xp[message.author.id].xp;
+  let curlevel = xp[message.author.id].level;
+  let nxtLevel = xp[message.author.id].level * 300;
+  xp[message.author.id].xp = curxp + xpAdd;
+  if(nxtLevel <= xp[message.author.id].xp){
+    xp[message.author.id].level = curlevel + 1;
+    let lvlup = new Discord.RichEmbed()
+    .setTitle("Level Up!")
+    .setColor(purple)
+    .addField("New Level", curlevel + 1);
+
+    message.channel.send(lvlup).then(msg => {msg.delete(5000)});
+  }
+  fs.writeFile("./xp.json", JSON.stringify(xp), (err) =>{
+    if(err) console.log(err)
+  });
+
+  //Level System END
 
   let prefix = botconfig.prefix;
   let messageArray = message.content.split(" ");
